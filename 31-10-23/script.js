65066f5f776518ff79aeb14fa005d6b871464503
giuseppeAnzalone <148492151+giuseppeAnzalone@users.noreply.github.com>
2023-11-01 20:08:31 +0100

    Add files via upload

---
+++ 31-10-23/script.js
@@ -0,0 +1,16 @@
+const animalList = [
+  "Cane",
+  "Gatto",
+  "Topo",
+  "Leone",
+  "Tigre",
+  "Orso",
+  "Pantera",
+  "Giraffa",
+];
+
+console.log(animalList);
+
+for (let i = 0; i < animalList.length; i++) {
+  console.log(i + ": " + animalList[i]);
+}
